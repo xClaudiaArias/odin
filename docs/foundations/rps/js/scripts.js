3dedc034ddcf6f3909b0179ab14b9cfada5f5bce
xClaudiaArias <xClaudiaArias@gmail.com>
2026-02-13 16:00:59 -0500

    dom manipulation choice works

--- docs/foundations/rps/js/scripts.js
+++ docs/foundations/rps/js/scripts.js
@@ -1,29 +1,24 @@
-// console.log("Hello World")
-const playBtn = document.getElementById('play-btn')
+const player_choice = document.getElementById('player-choice')
+const computer_choice = document.getElementById('computer-choice')
 const choiceBtn = document.querySelectorAll(".choice-btn");
+const displa = document.getElementById(".display");
 
 const getComputerChoice = () => {
 
     let computerChoice = Math.floor(Math.random() * 3)
-    console.log(computerChoice)
+    console.log(computerChoice, " :computerChoice")
 
     switch (computerChoice) {
         case 0:
-            console.log('rock')
+            console.log('rock', " :computerChoice")
             return 'rock';
         case 1:
-            console.log('paper')
+            console.log('paper', " :computerChoice")
             return 'paper';
         case 2:
-            console.log('scissors')
+            console.log('scissors', " :computerChoice")
             return 'scissors';
     }
-}
-
-const getHumanChoice = () => {
-    let humanChoice = prompt("rock, paper or scissors?")
-    console.log('humanChoice: ', humanChoice)
-    return humanChoice.toLowerCase()
 }
 
 function playGame () {
@@ -33,38 +28,49 @@ function playGame () {
     const playRound = (humanChoice, computerChoice) => {
 
         if ((humanChoice === 'paper' && computerChoice === 'rock') || (humanChoice === 'rock' && computerChoice === 'scissors') || (humanChoice === 'scissors' && computerChoice === 'paper') ) {
+            computer_choice.innerHTML = "Computer's choice: " + computerChoice
+            player_choice.innerHTML = "Your choice: " + humanChoice
+            display.innerHTML = `You Win! ${humanChoice} beats ${computerChoice}`
             console.log(`You Win! ${humanChoice} beats ${computerChoice}`)
             return  humanScore += 1
         } else if ((computerChoice === 'paper' && humanChoice === 'rock') || (computerChoice === 'rock' && humanChoice === 'scissors') || (computerChoice === 'scissors' && humanChoice === 'paper')) {
+            computer_choice.innerHTML = "Computer's choice: " + computerChoice
+            player_choice.innerHTML = "Your choice: " + humanChoice
+            display.innerHTML = `You lose! ${computerChoice} beats ${humanChoice}`
             console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
             return computerScore += 1
         } else {
+            computer_choice.innerHTML = "Computer's choice: " + computerChoice
+            player_choice.innerHTML = "Your choice: " + humanChoice
+            display.innerHTML = 'It\'s a tie'
             console.log('It\'s a tie')
             return ''
         }
     }
 
-    const humanSelection = getHumanChoice()
-    const computerSelection = getComputerChoice()
+    choiceBtn.forEach(btn => btn.addEventListener('click', () => {
+            const computerSelection = getComputerChoice()
+            const humanSelection = btn.innerHTML.toLowerCase();
+            console.log(humanSelection, " :humanSelection")
+            playRound(humanSelection, computerSelection)
+        }
+    ))
+    
 
-    playRound(humanSelection, computerSelection)
-
-
-    console.log(`SCORE ---- HUMAN: ${humanScore} COMPUTER: ${computerScore}`)
-    if (humanScore > computerScore) {
-        alert(`Human wins!!! Human: ${humanScore} > Computer: ${computerScore}`)
-        console.log('Human wins')
-    } else if (computerScore > humanScore) {
-        alert(`Computer wins!!! Computer:  ${computerScore} > Human: ${humanScore}`)
-        console.log('Computer wins')
-    } else {
-        alert(`it\'s a tie!!! Human: ${humanScore}, Computer: ${computerScore}`)
-        console.log('it\'s a tie')
-    }
+    // console.log(`SCORE ---- HUMAN: ${humanScore} COMPUTER: ${computerScore}`)
+    // if (humanScore > computerScore) {
+    //     alert(`Human wins!!! Human: ${humanScore} > Computer: ${computerScore}`)
+    //     console.log('Human wins')
+    // } else if (computerScore > humanScore) {
+    //     alert(`Computer wins!!! Computer:  ${computerScore} > Human: ${humanScore}`)
+    //     console.log('Computer wins')
+    // } else {
+    //     alert(`it\'s a tie!!! Human: ${humanScore}, Computer: ${computerScore}`)
+    //     console.log('it\'s a tie')
+    // }
 }
 
-playBtn.addEventListener('click', () => {
-    playGame()
-})
-
+// Create three buttons, one for each selection. 
+// Add an event listener to the buttons that call your playRound function with the correct playerSelection every time a button is clicked. (you can keep the console.logs for this step)
+playGame()
 
